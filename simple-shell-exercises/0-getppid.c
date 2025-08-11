adaea31196c8c01987137d1d6e3dfc54c8a0eba7
“cryspycreme” <11529@holbertonstudents.com>
2025-08-11 01:27:45 +0000

    Add scripts and exercises for getppid

---
+++ simple-shell-exercises/0-getppid.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - PPID
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	pid_t my_ppid;
+	
+	my_ppid = getppid();
+	printf("%u\n", my_ppid);
+	return (0);
+}
