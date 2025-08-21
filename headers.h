b33f67eaa35e8eb0b19b7a9d1367091a9b53edf2
“cryspycreme” <11529@holbertonstudents.com>
2025-08-21 06:51:27 +0000

    Add header file

---
+++ headers.h
@@ -0,0 +1,15 @@
+#ifndef header_file
+#define header_file
+
+#include <stdio.h>     
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+char *_getenv(const char *name);
+char *find_path(char *command);
+
+#endif
