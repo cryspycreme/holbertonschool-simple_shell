1fd5b641e24862848cfc3f821314f91cacff4fa1
“cryspycreme” <11529@holbertonstudents.com>
2025-08-21 09:44:28 +0000

    Simple shell 0.1

--- simple_shell.c
+++ simple_shell.c
@@ -9,116 +9,92 @@
 #define MAX_ARGS 100
 extern char **environ;
 
-int main(void)
+int main(int argc, char **argv)
 {
-size_t size = 0;
-ssize_t ncread;
-char *line = NULL, *input_copy, *token, **command, *full_path;
-pid_t child;
-int i, status, execute, interactive = isatty(STDIN_FILENO);
+    size_t size = 0;
+    ssize_t ncread;
+    char *line = NULL, *input_copy, *token, **command;
+    pid_t child;
+    int i, status, execute, interactive = isatty(STDIN_FILENO);
 
-	while (1)
-	{
-		if (interactive == 1)
-		write(1, "$ ", 2);
+	(void)argc;
+    while (1)
+    {
+        if (interactive == 1)
+            write(1, "$ ", 2);
 
-		ncread = getline(&line, &size, stdin);
+        ncread = getline(&line, &size, stdin);
 
-		if (ncread == -1)
-		{
-			if (interactive == 1)
-			{
-				write(1, "\n", 1);
-			}
-			break;
-		}
+        if (ncread == -1)
+        {
+            if (interactive == 1)
+            {
+                write(1, "\n", 1);
+            }
+            break;
+        }
 
-		input_copy = strdup(line);
+        input_copy = strdup(line);
+        if (input_copy == NULL)
+        {
+            perror("strdup");
+            continue;
+        }
 
-		if (input_copy == NULL)
-		{
-			perror("strdup");
-			goto cleanup;
-		}
+        if (ncread > 0 && input_copy[ncread - 1] == '\n')
+        {
+            input_copy[ncread - 1] = '\0';
+        }
 
-		if (ncread > 0 && input_copy[ncread - 1] == '\n')
-		{
-			input_copy[ncread - 1] = '\0';
-		}
+        i = 0;
+        token = strtok(input_copy, " \t\n\r");
 
-		i = 0;
+        command = malloc(sizeof(char *) * MAX_ARGS);
+        if (command == NULL)
+        {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+        while (token != NULL)
+        {
+            command[i] = token;
+            token = strtok(NULL, " \t\n\r");
+            i++;
+        }
+        command[i] = NULL;
 
-		token = strtok(input_copy, " \t\n\r");
+        if (command[0] == NULL)
+        {
+            free(command);
+            free(input_copy);
+            continue;
+        }
 
-		command = malloc(sizeof(char *) * MAX_ARGS);
-		if (command == NULL)
-		{
-			perror("malloc");
-			goto cleanup;
-		}
-
-		while (token != NULL)
-		{
-			command[i] = token;
-			token = strtok(NULL, " \t\n\r");
-			i++;
-		}
-		command[i] = NULL;
-
-		if (command[0] == NULL)
-		{
-			goto cleanup;
-		}
-
-		full_path = find_path(command[0]);
-
-		if (full_path == NULL)
-		{
-			dprintf(STDERR_FILENO, "Command not found: %s\n", command[0]);
-			goto cleanup;
-		}
-
-		child = fork();
-
-		if (child < 0)
-		{
-			perror("fork");
-			goto cleanup;
-		}
-
-		if (child == 0)
-		{
-			execute = execve(full_path, command, environ);
-			if (execute == -1)
-			{
-				perror("execve");
-				return (EXIT_FAILURE);
-			}
-		}
-		else
-		{
-		goto cleanup;	if (waitpid(child, &status, 0) == -1)
-			{
-				perror("waitpid");
-			}
-		}
-	cleanup:
-		if (full_path)
-		{
-			free(full_path);
-			full_path = NULL;
-		}
-		if (command)
-		{
-			free(command);
-			command = NULL;
-		}
-		if (input_copy)
-		{
-			free(input_copy);
-			input_copy = NULL;
-		}
-	}
-	free(line);
-	return (0);
+        child = fork();
+        if (child < 0)
+        {
+            perror("fork");
+            return (EXIT_FAILURE);
+        }
+        if (child == 0)
+        {
+            execute = execve(command[0], command, environ);
+            if (execute == -1)
+            {
+                dprintf(STDERR_FILENO, "%s: 1: %s: not found\n", argv[0], command[0]);
+                return (EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            if (waitpid(child, &status, 0) == -1)
+            {
+                perror("waitpid");
+            }
+        }
+        free(command);
+        free(input_copy);
+    }
+    free(line);
+    return (0);
 }
