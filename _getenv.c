72d854c7a0c321498691797c11e2998e9af64de7
“cryspycreme” <11529@holbertonstudents.com>
2025-08-23 07:53:48 +0000

    Fix betty style errors

--- _getenv.c
+++ _getenv.c
@@ -9,21 +9,18 @@
  * Return: Pointer to the content of a variable, or NULL if fails.
  */
 
-extern char **environ;
-
 char *_getenv(const char *name)
 {
-    int i = 0;
-    int name_len = strlen(name);
+	int i = 0;
+	int name_len = strlen(name);
 
-    while (environ[i] != NULL)
-    {
-        if (environ[i][name_len] == '=' && strncmp(environ[i], name, name_len) == 0)
-        {
-            return (environ[i] + name_len + 1);
-        }
-        i++;
-    }
-
-    return (NULL);
+	while (environ[i] != NULL)
+	{
+		if (environ[i][name_len] == '=' && strncmp(environ[i], name, name_len) == 0)
+		{
+			return (environ[i] + name_len + 1);
+		}
+		i++;
+	}
+	return (NULL);
 }
