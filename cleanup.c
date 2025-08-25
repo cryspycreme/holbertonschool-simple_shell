4578b8e8144b9bcaadcdfa14802650c24da289fc
“cryspycreme” <11529@holbertonstudents.com>
2025-08-25 04:07:21 +0000

    Set pointers to NULL after being freed so avoid double free

--- cleanup.c
+++ cleanup.c
@@ -12,13 +12,16 @@ void cleanup(char *full_path, char **command, char *input_copy)
 		if (full_path)
 		{
 			free(full_path);
+			full_path = NULL;
 		}
 		if (command)
 		{
 			free(command);
+			command = NULL;
 		}
 		if (input_copy)
 		{
 			free(input_copy);
+			input_copy = NULL;
 		}
 }
