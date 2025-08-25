e835903a0171d9148d35db5bade74eb8d9ee49d0
“cryspycreme” <11529@holbertonstudents.com>
2025-08-25 04:20:37 +0000

    Cleanup on exit and cleanup on built-in handled

--- simple_shell.c
+++ simple_shell.c
@@ -33,11 +33,12 @@ int main(int argc, char *argv[])
 			int b = builtins_flow(command, line, input_copy, &exit_code);
 
 			if (b == -1)
+				cleanup(NULL, command, input_copy);
 				break;
 			if (b == 1)
+				cleanup(NULL, command, input_copy);
 				continue;
 		}
-
 		execute_flow(command, argv[0], input_copy, &exit_code);
 	}
 	free(line);
